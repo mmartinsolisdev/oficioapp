d19c258fe5621f6377a87ad110d8da7f9d6ab07e
mmartinsolis.dev@gmail.com <mmartinsolis.dev@gmail.com>
2018-09-06 01:41:46 -0500

    Se agregó Mongoose, Body-Parser y Cors al proyecto, el puerto y los Headers

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 'use strict';
-const express = require ("express");
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const cors = require ('cors');
+const mongoose = require ('mongoose');
 
 const app = express();
 
-const port = 3000;
+// Usar este puerto si la app será alojada en Heroku
+var port = process.env.PORT || 8080;
+// Usar este puerto para desarrollo
+// const port = 3000;
+
+// CORS Middleware permite la conexion entre archivos
+app.use(cors());
+
+// Establecer carpeta
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Body Parser Middleware permite la renderización de las vistas
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+//Headers necesarios para que el servidor node de accso a las peticiones no importando que cabeceras
+/*app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });*/
 
 // Index Route
 app.get('/', (req,res) => {
